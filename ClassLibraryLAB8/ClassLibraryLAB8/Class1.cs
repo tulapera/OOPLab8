96ed0e9c5d7f17a29f7b3010cf110f9c0269fcf5
Віталій <154689439+tulapera@users.noreply.github.com>
2024-12-20 18:41:27 +0200

    Add files via upload

---
+++ ClassLibraryLAB8/ClassLibraryLAB8/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Бібліотека класів
+namespace VehicleLibrary
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public double Mileage { get; set; }
+        public int Power { get; set; }
+        public int Cylinders { get; set; }
+
+        public Vehicle() { }
+
+        public Vehicle(string brand, double mileage, int power, int cylinders)
+        {
+            Brand = brand;
+            Mileage = mileage;
+            Power = power;
+            Cylinders = cylinders;
+        }
+
+        public Vehicle(Vehicle other)
+        {
+            Brand = other.Brand;
+            Mileage = other.Mileage;
+            Power = other.Power;
+            Cylinders = other.Cylinders;
+        }
+
+        public virtual void ChangePower(int newPower)
+        {
+            Power = newPower;
+        }
+
+        public virtual void ChangeMileage(double newMileage)
+        {
+            Mileage = newMileage;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Марка: {Brand}, Пробіг: {Mileage}, Потужність: {Power}, Кількість циліндрів: {Cylinders}");
+        }
+    }
+
+    public class Lorry : Vehicle
+    {
+        public double LoadCapacity { get; set; }
+
+        public Lorry() : base() { }
+
+        public Lorry(string brand, double mileage, int power, int cylinders, double loadCapacity) : base(brand, mileage, power, cylinders)
+        {
+            LoadCapacity = loadCapacity;
+        }
+
+        public Lorry(Lorry other) : base(other)
+        {
+            LoadCapacity = other.LoadCapacity;
+        }
+
+        public void ChangeLoadCapacity(double newCapacity)
+        {
+            LoadCapacity = newCapacity;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Вантажопідйомність: {LoadCapacity}");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public int PassengerCount { get; set; }
+
+        public Car() : base() { }
+
+        public Car(string brand, double mileage, int power, int cylinders, int passengerCount) : base(brand, mileage, power, cylinders)
+        {
+            PassengerCount = passengerCount;
+        }
+
+        public Car(Car other) : base(other)
+        {
+            PassengerCount = other.PassengerCount;
+        }
+
+        public void ChangePassengerCount(int newCount)
+        {
+            PassengerCount = newCount;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Кількість пасажирів: {PassengerCount}");
+        }
+    }
+}
---
+++ OOPLab8/OOPLab8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    using VehicleLibrary;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<Vehicle> vehicles = new List<Vehicle>
+            {
+                new Lorry("Volvo", 200000, 400, 6, 15000),
+                new Car("Toyota", 50000, 150, 4, 5)
+            };
+
+            Console.WriteLine("Інформація про транспортні засоби:");
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.ShowInfo();
+                Console.WriteLine();
+            }
+
+            Vehicle car1 = new Car("Honda", 30000, 120, 4, 4);
+            Vehicle lorry1 = new Lorry("Scania", 250000, 450, 6, 20000);
+
+            Console.WriteLine("Оновлення інформації...");
+            car1.ChangeMileage(35000);
+            lorry1.ChangePower(480);
+
+            Console.WriteLine("Оновлена інформація про транспортні засоби:");
+            car1.ShowInfo();
+            Console.WriteLine();
+            lorry1.ShowInfo();
+        }
+    }
+}
